18242df517f7b46d8752f9c4bca703f881542740
Sarbjyotsngh <chahalsarbjyotsingh@gmail.com>
2019-12-26 20:15:50 +0530

    Adding Spinner when user wait for fetching location and weather data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
+import 'package:clima/screens/location_screen.dart';
 import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apiKey = '9d868bd4b3091ce45569039efaf4191f';
 
@@ -25,10 +27,21 @@ class _LoadingScreenState extends State<LoadingScreen> {
     NetworkHelper networkHelper = new NetworkHelper(
         'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
     var weatherData = await networkHelper.getData();
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) {
+        return LocationScreen();
+      }),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SpinKitDoubleBounce(
+        size: 70.0,
+        color: Colors.white,
+      ),
+    );
   }
 }
